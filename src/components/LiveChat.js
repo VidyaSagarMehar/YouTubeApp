9cf9eb3debce444500101b4be4d4a90566ea8df7
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-03-15 00:39:51 +0530

    Added - chat functionality when you hit enter on th input box

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/chatSlice';
 import { getRandomName, makeRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
+	const [liveMessage, setLiveMessage] = useState('');
+
 	// dispatching the action
 	const dispatch = useDispatch();
 
@@ -13,7 +15,7 @@ const LiveChat = () => {
 	useEffect(() => {
 		const interval = setInterval(() => {
 			// API polling
-			console.log('API polling');
+			// console.log('API polling');
 
 			dispatch(
 				addMessage({
@@ -28,11 +30,41 @@ const LiveChat = () => {
 	}, []);
 
 	return (
-		<div className="h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-md overflow-y-scroll flex flex-col-reverse">
-			{ChatMessages.map((c, i) => (
-				<ChatMessage key={i} name={c.name} message={c.message} />
-			))}
-		</div>
+		<>
+			<div className="h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-md overflow-y-scroll flex flex-col-reverse">
+				{ChatMessages.map((c, i) => (
+					<ChatMessage key={i} name={c.name} message={c.message} />
+				))}
+			</div>
+			<form
+				className="w-full flex justify-around border border-black p-2 "
+				onSubmit={(e) => {
+					e.preventDefault();
+					console.log('submit');
+					dispatch(
+						addMessage({
+							name: 'Vidya Sagar',
+							message: liveMessage,
+						}),
+					);
+				}}
+			>
+				<input
+					className="border border-black rounded w-96 px-1"
+					type="text"
+					value={liveMessage}
+					onChange={(e) => {
+						setLiveMessage(e.target.value);
+					}}
+				/>
+				<button
+					className="border border-black rounded px-3 bg-green-100"
+					type="submit"
+				>
+					Submit
+				</button>
+			</form>
+		</>
 	);
 };
 
--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { LIVE_CHAT_COUNT } from './constants';
 
 const chatSlice = createSlice({
 	name: 'chat',
@@ -9,7 +10,7 @@ const chatSlice = createSlice({
 		addMessage: (state, action) => {
 			// it will push at the end
 			// state.messages.push(action.payload);
-
+			state.messages.splice(LIVE_CHAT_COUNT, 1);
 			// it will push it at the begining
 			state.messages.unshift(action.payload);
 		},
