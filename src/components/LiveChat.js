0d09b5181b0ffb58d136f5c3c6e319f80ff31dc3
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-03-14 22:39:53 +0530

    Inital setup for chat reducer

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ChatMessage from './ChatMessage';
 
 const LiveChat = () => {
+	useEffect(() => {
+		const i = setInterval(() => {
+			// API polling
+			console.log('API polling');
+		}, 2000);
+
+		//clear interval
+		return () => clearInterval(i);
+	}, []);
+
 	return (
 		<div className="h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-md">
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
-			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
 			<ChatMessage name="Vidya Sagar" message="Hello from live chat 👍" />
 		</div>
 	);
